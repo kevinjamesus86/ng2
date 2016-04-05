dce10a073fe4083eaf90ea49ef159e97b2e3d0d4
kevin.gay <kevin.gay@bodybuilding.com>
2016-04-04 19:50:50 -0700

    refactor drop

--- app/src/drop/drop.component.ts
+++ app/src/drop/drop.component.ts
@@ -1,62 +1,66 @@
 import {
     Component,
+    Directive,
     Input,
     Output,
-    EventEmitter
+    EventEmitter,
+    ElementRef
 } from 'angular2/core';
 
-import { DropItem } from './drop-item.component';
+import { BrowserDomAdapter } from 'angular2/platform/browser';
 
 ///////////////
 ///////////////
+
+
+// Drop
 
 @Component({
     selector: 'drop',
     templateUrl: 'app/src/drop/drop.ng.html'
 })
+
 export class Drop {
     show: boolean = false;
-    item: DropItem = null;
     items: Array<DropItem> = [];
+    selectedItem: DropItem = null;
 
-    @Input()
-    value = null;
-    @Output()
-    valueChange = new EventEmitter();
+    @Input() value: any = null;
+    @Output() valueChange = new EventEmitter();
 
     add(item: DropItem) {
         this.items.push(item);
 
-        // Drop item value matches the current [value] binding,
-        // select it without emitting a value change event
         if (item.value === this.value) {
             this.select(item, false);
         }
     }
 
     remove(item: DropItem) {
-
         let index = this.items.indexOf(item);
         if (~index) {
             this.items.splice(index, 1);
         }
 
         // Item is currently selected, unset it
-        if (this.item === item) {
-            this.item = null;
+        if (this.selectedItem === item) {
+            this.selectedItem = null;
         }
-
     }
 
-    select(nextItem: DropItem, emit: boolean = true) {
-        this.item = nextItem;
+    select(item: DropItem, emit: boolean = true) {
+        if (this.selectedItem !== item) {
+            this.selectedItem = item;
 
-        // update [value] binding
-        emit && this.valueChange.emit(nextItem.value);
+            // update [value] binding
+            if (emit) {
+                this.valueChange.emit(item.value);
+            }
 
-        // update items selection state
-        for (let item of this.items) {
-            item.selected = item === nextItem;
+            // update items selection state
+            for (let _item of this.items) {
+                _item.selected = _item === item;
+            }
         }
     }
 
@@ -68,6 +72,9 @@ export class Drop {
         this.show = !this.show;
     }
 
+    // todo: better matching when value changes. if the reference
+    // changes how do we determine if it is the "same" or not?
+    // something like `track by` to know what to compare to would be handy
     ngOnChanges(changes) {
         let match;
         let value = changes.value;
@@ -82,8 +89,84 @@ export class Drop {
                 }
             }
             if (!match) {
-                this.item = null;
+                this.selectedItem = null;
             }
         }
     }
+}
+
+
+// Drop Closer
+
+@Directive({
+    selector: 'drop',
+    host: {
+        '(click)': 'trackEvent($event)',
+        '(document:click)': 'verifyEvent($event)'
+    }
+})
+
+export class DropCloser {
+    lastEvent: MouseEvent = null;
+
+    constructor(private drop: Drop) {}
+
+    trackEvent(event: MouseEvent) {
+        this.lastEvent = event;
+    }
+
+    verifyEvent(event: MouseEvent) {
+        let lastEvent = this.lastEvent;
+        this.lastEvent = null;
+
+        if (lastEvent !== event) {
+            this.drop.hideMenu();
+        }
+    }
+
+    ngOnDestroy() {
+        this.lastEvent = null;
+    }
+
+}
+
+
+// Drop Item
+
+@Component({
+    selector: 'drop-item',
+    templateUrl: 'app/src/drop/drop-item.ng.html'
+})
+
+export class DropItem {
+    dom: BrowserDomAdapter;
+    selected: boolean = false;
+
+    @Input() value: any = null;
+    @Input('label') _label: string = '';
+
+    constructor(
+        private drop: Drop,
+        private element: ElementRef
+    ) {
+        this.dom = new BrowserDomAdapter();
+    }
+
+    get label() : string {
+        return this._label || this.dom.getText(this.element.nativeElement).trim();
+    }
+
+    onClick() {
+        this.drop.select(this);
+    }
+
+    ngOnInit() {
+        this.drop.add(this);
+    }
+
+    ngOnDestroy() {
+        this.drop.remove(this);
+        this.drop = this.dom = null;
+    }
+
 }
--- app/src/drop/drop.ts
+++ app/src/drop/drop.ts
@@ -1,8 +1,12 @@
 import { Type } from 'angular2/core';
 
-import { Drop } from './drop.component';
-import { DropCloser } from './drop-closer.directive';
-import { DropItem } from './drop-item.component';
+import {
+    Drop,
+    DropCloser,
+    DropItem
+} from './drop.component';
+
+export * from './drop.component';
 
 export const DROP_DIRECTIVES: Type[] = [
     Drop,
